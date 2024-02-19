a4ecb8c316483ca70b855049ab213f17f6f49ffc
erankrs <eran.karisi@gmail.com>
2024-02-19 11:10:14 +0200

    19-02

--- TechNova/JiraConnector.ts
+++ TechNova/JiraConnector.ts
@@ -7,7 +7,7 @@ const apiUrl = 'https://eran-karisi.atlassian.net/rest/agile/1.0/board/{boardId}
 
 // Define your Jira API credentials (replace with your actual credentials)
 const username = 'eran.karisi@gmail.com';
-const apiToken = 'ATATT3xFfGF0onT55qMG4T5eSp2JBwS9Hhwph_gfRWYnfq4VjKxhoCZaXDgSq-dL6mGTrr5_jaD48ivfcgU9kZkv5TPGlznngJPPfJXk0MIQQX0lP0Q8BbjRzR_3Ng-ZAMJZYDtwQ9tieNqWB5YYIbdjKpFbZmsP0RVr7ytFHpZW5HwaEXt4Dfc=C89FDF55';
+const apiToken = 'ATATT3xFfGF0QdkAIUAcrJF8ezPQiqe1oa-2-fF8d3jvLBKub4W_JcHxWTq0cldRRpSaLUslzI5EQpGZHZvpNKgre31PsMS4rLOV0lCSFm13TOV-U_2poCn09HZB05Yza3CamATTIuRD-Ht96pnaib85KIGXp5njBa_OZ4Gv4OzErqxZ2pT-_9s=42278A67';
 
 // Define the board ID of the Jira board containing the sprints (replace with your actual board ID)
 const boardId = '1';
@@ -21,6 +21,7 @@ const headers = {
 // Make the API call to fetch all sprints data
 axios.get(apiUrl.replace('{boardId}', boardId), { headers })
   .then((response: AxiosResponse) => {
+    console.log("CCDDD ",response.headers);
     console.log('All sprints data:', response.data.values);
   })
   .catch((error) => {
--- dist/TechNova/JiraConnector.js
+++ dist/TechNova/JiraConnector.js
@@ -7,7 +7,7 @@ function fetchSprintData() {
     var apiUrl = 'https://eran-karisi.atlassian.net/rest/agile/1.0/board/{boardId}/sprint';
     // Define your Jira API credentials (replace with your actual credentials)
     var username = 'eran.karisi@gmail.com';
-    var apiToken = 'ATATT3xFfGF0onT55qMG4T5eSp2JBwS9Hhwph_gfRWYnfq4VjKxhoCZaXDgSq-dL6mGTrr5_jaD48ivfcgU9kZkv5TPGlznngJPPfJXk0MIQQX0lP0Q8BbjRzR_3Ng-ZAMJZYDtwQ9tieNqWB5YYIbdjKpFbZmsP0RVr7ytFHpZW5HwaEXt4Dfc=C89FDF55';
+    var apiToken = 'ATATT3xFfGF0QdkAIUAcrJF8ezPQiqe1oa-2-fF8d3jvLBKub4W_JcHxWTq0cldRRpSaLUslzI5EQpGZHZvpNKgre31PsMS4rLOV0lCSFm13TOV-U_2poCn09HZB05Yza3CamATTIuRD-Ht96pnaib85KIGXp5njBa_OZ4Gv4OzErqxZ2pT-_9s=42278A67';
     // Define the board ID of the Jira board containing the sprints (replace with your actual board ID)
     var boardId = '1';
     // Define the headers for authentication
@@ -18,6 +18,7 @@ function fetchSprintData() {
     // Make the API call to fetch all sprints data
     axios_1.default.get(apiUrl.replace('{boardId}', boardId), { headers: headers })
         .then(function (response) {
+        console.log("CCDDD ", response.headers);
         console.log('All sprints data:', response.data.values);
     })
         .catch(function (error) {
